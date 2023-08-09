3e079774b82022111ea1ece0483335d405761ed3
sgnahouidavid1 <derrickdavid32200@gmail.com>
2023-08-08 22:56:13 -0400

    Added elements to the signup page and the login page, size buttons and fix text

--- app/src/main/java/com/example/womens_security_with_sms_alerts/LoginActivity.kt
+++ app/src/main/java/com/example/womens_security_with_sms_alerts/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.womens_security_with_sms_alerts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val tvRegisterButton: TextView = findViewById(R.id.tvRegisterButton)
+        tvRegisterButton.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/womens_security_with_sms_alerts/SignUpActivity.kt
+++ app/src/main/java/com/example/womens_security_with_sms_alerts/SignUpActivity.kt
@@ -4,16 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-        val btLogin: Button = findViewById(R.id.btLogin)
-        btLogin.setOnClickListener {
+
+        val tvLoginButton: TextView = findViewById(R.id.tvLoginButton)
+
+        tvLoginButton.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
+
     }
 }
